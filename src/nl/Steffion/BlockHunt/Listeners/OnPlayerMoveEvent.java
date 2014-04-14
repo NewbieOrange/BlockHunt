32fc1d351edae38834ce885df16aa7697ac80dee
NewbieOrange <yutiancheng1999@gmail.com>
2014-04-14 22:53:29 +0800

    Some changes

--- src/nl/Steffion/BlockHunt/Listeners/OnPlayerMoveEvent.java
+++ src/nl/Steffion/BlockHunt/Listeners/OnPlayerMoveEvent.java
@@ -1,10 +1,15 @@
 package nl.Steffion.BlockHunt.Listeners;
 
 import nl.Steffion.BlockHunt.Arena;
+import nl.Steffion.BlockHunt.ArenaHandler;
+import nl.Steffion.BlockHunt.ConfigC;
+import nl.Steffion.BlockHunt.DisguiseDelegate;
 import nl.Steffion.BlockHunt.Arena.ArenaState;
+import nl.Steffion.BlockHunt.Managers.MessageM;
 import nl.Steffion.BlockHunt.W;
 
 import org.bukkit.Effect;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -15,7 +20,8 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class OnPlayerMoveEvent implements Listener {
 
-	@EventHandler(priority = EventPriority.NORMAL)
+	@SuppressWarnings("deprecation")
+    @EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerMoveEvent(PlayerMoveEvent event) {
 		Player player = event.getPlayer();
 
@@ -63,11 +69,70 @@ public class OnPlayerMoveEvent implements Listener {
 						player.playSound(loc, Sound.GHAST_FIREBALL, 1, 1);
 						player.teleport(arena.hidersWarp);
 					} else if (loc.getBlockY() < minY) {
-						player.damage(100.00);
-					    event.setCancelled(true);
+					    if (!arena.seekers.contains(player))
+                        {
+                            if (W.shop.getFile().get(
+                                    player.getName() + ".tokens") == null)
+                            {
+                                W.shop.getFile().set(
+                                        player.getName() + ".tokens", 0);
+                                W.shop.save();
+                            }
+                            int playerTokens = W.shop.getFile().getInt(
+                                    player.getName() + ".tokens");
+                            float addingTokens = ((float) arena.hidersTokenWin - (((float) arena.timer / (float) arena.gameTime) * (float) arena.hidersTokenWin));
+                            W.shop.getFile().set(
+                                    player.getName() + ".tokens",
+                                    playerTokens + (int) addingTokens);
+                            W.shop.save();
+
+                            MessageM.sendFMessage(player,
+                                    ConfigC.normal_addedToken, "amount-"
+                                            + (int) addingTokens);
+
+                            arena.seekers.add(player);
+                            player.setFoodLevel(20);
+                            ArenaHandler
+                                    .sendFMessage(
+                                            arena,
+                                            ConfigC.normal_ingameHiderDied,
+                                            "playername-"
+                                                    + player.getName(),
+                                            "left-"
+                                                    + (arena.playersInArena
+                                                            .size() - arena.seekers
+                                                            .size()));
+                        }
+                        else
+                        // seeker die
+                        {
+                            ArenaHandler.sendFMessage(arena,
+                                    ConfigC.normal_ingameSeekerDied,
+                                    "playername-" + player.getName(),
+                                    "secs-" + arena.waitingTimeSeeker);
+                        }
+                        player.getInventory().clear();
+                        player.updateInventory();
+
+                        if (arena.seekers.size() >= arena.playersInArena
+                                .size())
+                        {
+                            ArenaHandler.seekersWin(arena);
+                        }
+                        else
+                        {
+                            // DisguiseAPI.undisguiseToAll(player);
+                            DisguiseDelegate.GetSingleton().UnDisguise(
+                                    player);
+                            W.seekertime.put(player,
+                                    arena.waitingTimeSeeker);
+                            player.teleport(arena.seekersWarp);
+                            player.setGameMode(GameMode.SURVIVAL);
+                        }
+					    //event.setCancelled(true);
 						player.playEffect(loc, Effect.ENDER_SIGNAL, null);
 						player.playSound(loc, Sound.GHAST_FIREBALL, 1, 1);
-						player.teleport(arena.hidersWarp);
+						//player.teleport(arena.hidersWarp);
 					}
 				}
 			}
