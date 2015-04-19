08516d3428c10d3a3c7aa1740b96d9f551aa39ae
NewbieOrange <yutiancheng1999@gmail.com>
2015-04-19 16:09:20 +0800

    Fix seekers' deaths.

--- src/nl/Steffion/BlockHunt/Listeners/OnPlayerDeathEvent.java
+++ src/nl/Steffion/BlockHunt/Listeners/OnPlayerDeathEvent.java
@@ -1,8 +1,14 @@
 package nl.Steffion.BlockHunt.Listeners;
 
 import nl.Steffion.BlockHunt.Arena;
+import nl.Steffion.BlockHunt.ArenaHandler;
+import nl.Steffion.BlockHunt.ConfigC;
+import nl.Steffion.BlockHunt.DisguiseDelegate;
 import nl.Steffion.BlockHunt.W;
-
+import org.bukkit.Effect;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -22,10 +28,26 @@ public class OnPlayerDeathEvent implements Listener
                 if (arena.playersInArena.contains(player)
                         && arena.seekers.contains(player))
                 {
-                    event.getDrops().clear();
                     event.setDroppedExp(0);
-                    player.setHealth(player.getMaxHealth());
-                    event.setDeathMessage(null);
+                    event.getDrops().clear();
+                    event.setKeepInventory(true);
+                    ArenaHandler.sendFMessage(arena, ConfigC.normal_ingameSeekerDied,
+                            "playername-" + player.getName(), "secs-"
+                                    + arena.waitingTimeSeeker);
+                    player.getInventory().clear();
+                    player.updateInventory();
+                    
+                    // DisguiseAPI.undisguiseToAll(player);
+                    DisguiseDelegate.GetSingleton().UnDisguise(player);
+                    W.seekertime.put(player, arena.waitingTimeSeeker);
+                    player.setHealth(20.0);
+                    player.teleport(arena.seekersWarp);
+                    player.setGameMode(GameMode.SURVIVAL);
+                    // event.setCancelled(true);
+                    Location loc = player.getLocation();
+                    player.playEffect(loc, Effect.ENDER_SIGNAL, null);
+                    player.playSound(loc, Sound.GHAST_FIREBALL, 1, 1);
+                    // player.teleport(arena.hidersWarp);
                 }
             }
         }
